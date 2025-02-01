eb0a1b81f90371ba3286a38c17f00bee2c750fe8
M1chal3k28 <balcerakm0@gmail.com>
2025-02-01 15:56:14 +0100

    added movement of the snake and changed character for head element

--- test.cpp
+++ test.cpp
@@ -2,35 +2,58 @@
 #include <iostream>
 #include <windows.h>
 #include <chrono>
+#include <conio.h>
 
 using namespace std;
 using namespace std::chrono_literals;
 
 #define snakeBodyASCII 186
+#define snakeBodyHorizontalASCII 205
 #define snakeTurningLeftUpASCII 187 
-#define snakeTurningLeftDownASCII 189 
+#define snakeTurningLeftDownASCII 188 
 #define snakeTurningRightDownASCII 200
 #define snakeTurningRightUpASCII 201
 #define backGroudASCII 32
 #define appleASCII 254
 
-void moveCursor(int x, int y) {
-    HANDLE consoleHandle = GetStdHandle((DWORD)STD_OUTPUT_HANDLE);
+bool getInput(char * key) { 
+    #ifdef _WIN32
+        if(kbhit()) {
+            *key = getch();    
+            return true;
+        }
 
-    SetConsoleCursorPosition(
-        consoleHandle,
-        COORD{(short)x, (short)y}
-    );
+        return false;
+    #else 
+        throw "Not implemented for this platform";
+    #endif
+}
+
+void moveCursor(int x, int y) {
+    #ifdef _WIN32
+        HANDLE consoleHandle = GetStdHandle((DWORD)STD_OUTPUT_HANDLE);
+
+        SetConsoleCursorPosition(
+            consoleHandle,
+            COORD{(short)x, (short)y}
+        );
+    #else 
+        throw "Not implemented for this platform";
+    #endif
 }
 
 void hideCursor() {
-    HANDLE hStdOut = NULL;
-    CONSOLE_CURSOR_INFO curInfo;
+    #ifdef _WIN32
+        HANDLE hStdOut = NULL;
+        CONSOLE_CURSOR_INFO curInfo;
 
-    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    GetConsoleCursorInfo(hStdOut, &curInfo);
-    curInfo.bVisible = FALSE;
-    SetConsoleCursorInfo(hStdOut, &curInfo);
+        hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        GetConsoleCursorInfo(hStdOut, &curInfo);
+        curInfo.bVisible = FALSE;
+        SetConsoleCursorInfo(hStdOut, &curInfo);
+    #else 
+        throw "Not implemented for this platform";
+    #endif
 }
 
 struct Position {
@@ -45,7 +68,7 @@ public:
     char body;
     SnakeBody * next;
     SnakeBody * prev;
-    int direction; // 0 - down; 1 - left; 2 - up; 3 - right;
+    int direction = 0; // 0 - down; 1 - left; 2 - up; 3 - right;
 
     SnakeBody(char body, SnakeBody * prev, vector<vector<char>> * grid, int y, int x) {
         this->body = body;
@@ -66,13 +89,24 @@ public:
         this->position.x = x;
         this->position.y = y;
         
-        this->gridPtr->data()[this->position.y][this->position.x] = snakeBodyASCII;
+        if (this->prev != nullptr) {
+            Position prevPos = this->prev->position;
+            // if we go to the left or right normally
+            if(prevPos.x - 1 == this->position.x || prevPos.x + 1 == this->position.x) {
+                this->body = snakeBodyHorizontalASCII;
+            }
+
+            // if we go to the up or down normally
+            if(prevPos.y - 1 == this->position.y || prevPos.y + 1 == this->position.y) {
+                this->body = snakeBodyASCII;
+            }
+        }
+        this->gridPtr->data()[this->position.y][this->position.x] = this->body;
         return _ret;
     }
 };
 
 void moveSnake(SnakeBody * head) {
-    SnakeBody * it = head;
     int prevX = head->position.x, prevY = head->position.y;
     switch(head->direction) {
         case 0: {
@@ -84,7 +118,7 @@ void moveSnake(SnakeBody * head) {
         } break;
 
         case 2: {
-            prevY += 1;
+            prevY -= 1;
         } break;
 
         case 3: {
@@ -92,12 +126,12 @@ void moveSnake(SnakeBody * head) {
         }
     }
 
-    while(it != nullptr) {
-        Position prevPos = it->move(prevX, prevY);
+    while(head != nullptr) {
+        Position prevPos = head->move(prevX, prevY);
         prevX = prevPos.x;
         prevY = prevPos.y;
 
-        it = it->next;
+        head = head->next;
     }
 }
 
@@ -107,25 +141,51 @@ int main() {
 
     vector<vector<char>> grid 
         = vector<vector<char>>
-        (10, vector<char>(20, char(backGroudASCII)));
+        (11, vector<char>(21, char(backGroudASCII)));
     
-    SnakeBody head = SnakeBody(char(snakeBodyASCII), nullptr, &grid, 1, 0);
+    for (int i = 0; i < 11; i++)
+        grid[i][20] = char(snakeBodyASCII);
+
+    for (int i = 0; i < 20; i++)
+        grid[10][i] = char(snakeBodyASCII);
+    
+    SnakeBody head = SnakeBody(char(appleASCII), nullptr, &grid, 1, 0);
     SnakeBody * it = &head;
-    it->next = new SnakeBody(char(snakeBodyASCII), it, &grid, 0, 0);
+    it->next = new SnakeBody(char(appleASCII), it, &grid, 0, 0);
+    it = it->next;
+    it->next = new SnakeBody(char(appleASCII), it, &grid, 0, 0);
+    it = it->next;
+    it->next = new SnakeBody(char(appleASCII), it, &grid, 0, 0);
     it = it->next;
 
     auto start = chrono::high_resolution_clock::now();
     auto updateTimerStart = start;
     while (true)
     {
+        // Check input
+        char key = ' ';
+        getInput(&key);
+        if (key == 'q') {
+            break;
+        } else if (key == 'w') {
+            head.direction = 2;
+        } else if (key == 'a') {
+            head.direction = 1;
+        } else if (key == 's') {
+            head.direction = 0;
+        } else if (key == 'd') {
+            head.direction = 3;
+        }
+
         // Update timer
         auto currTime = chrono::high_resolution_clock::now();
         auto timePassed = currTime - updateTimerStart;
-        if(chrono::duration_cast<chrono::seconds>(timePassed).count() >= 1.f) {
+        if(chrono::duration_cast<chrono::milliseconds>(timePassed).count() >= ((head.direction == 0 || head.direction == 2) ? 300.f : 200.f)) {
             moveSnake(&head); 
             updateTimerStart = currTime;
         }
 
+        // Draw
         moveCursor(0, 0);
         for (auto row : grid) {
             for (auto el : row) {
